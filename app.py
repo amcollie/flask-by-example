1d70bfe051860aa8e023e52d10d6b3f23175fe21
Alexandros Collie <amcollie@gmail.com>
2020-11-17 04:31:57 -0500

    added postgresql configuration

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask
-from dotenv import load_dotenv, find_dotenv
-
 import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('APP_SETTINGS'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wordcount_app:superfly73@0.0.0.0:5432/wordcount_dev'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-print(os.environ.get('APP_SETTINGS'))
+db = SQLAlchemy(app)
+
+from models import Result
+print(os.environ.get('DATABASE_URL'))
 @app.route('/')
 def hello():
     return "Hello World!"
