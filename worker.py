4ead11106a33119d34c0dd7d0cc40e6fb8e209c6
Alexandros Collie <amcollie@gmail.com>
2020-11-19 02:16:37 -0500

    modified worker.py to use redis string url

--- worker.py
+++ worker.py
@@ -4,11 +4,9 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-redis_host = os.getenv('REDISTOGO_HOST', 'localhost')
-redis_port = os.getenv('REDISTOGO_PORT', 6379)
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
-
-redis_client = Redis(host=redis_host, port=redis_port)
+redis_client = Redis().from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(redis_client):
